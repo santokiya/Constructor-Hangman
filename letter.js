38dfda17033e594019a09901fd2b4e0401a062c0
santokiya <santokiya@gmail.com>
2018-01-14 10:55:57 -0600

    added files

---
+++ letter.js
@@ -0,0 +1,5 @@
+// function that determines if a letter is visible or not
+// set the display state to reveal correctly guessed letters and to hide unguessed with lines
+
+
+module.exports = letter;
---
+++ word.js
@@ -0,0 +1,6 @@
+var letter = require("./letter.js");
+
+
+//function to pass a word to the user 
+
+module.exports = word;
